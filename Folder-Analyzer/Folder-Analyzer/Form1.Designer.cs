1833fa510a31efdc6863ffeb5b7b94e0dff79485
itought <mountyjapan24@gmail.com>
2025-11-20 14:08:52 +0200

    File Organizer

--- Folder-Analyzer/Folder-Analyzer/Form1.Designer.cs
+++ Folder-Analyzer/Folder-Analyzer/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PathInput = new System.Windows.Forms.TextBox();
+            this.RichBox = new System.Windows.Forms.RichTextBox();
+            this.AnalyzeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PathInput
+            // 
+            this.PathInput.Location = new System.Drawing.Point(151, 163);
+            this.PathInput.Multiline = true;
+            this.PathInput.Name = "PathInput";
+            this.PathInput.Size = new System.Drawing.Size(236, 31);
+            this.PathInput.TabIndex = 0;
+            this.PathInput.TextChanged += new System.EventHandler(this.PathInput_TextChanged);
+            // 
+            // RichBox
+            // 
+            this.RichBox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.RichBox.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RichBox.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.RichBox.Location = new System.Drawing.Point(12, 200);
+            this.RichBox.Name = "RichBox";
+            this.RichBox.Size = new System.Drawing.Size(705, 823);
+            this.RichBox.TabIndex = 1;
+            this.RichBox.Text = "";
+            this.RichBox.TextChanged += new System.EventHandler(this.RichBox_TextChanged);
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AnalyzeButton.Location = new System.Drawing.Point(37, 163);
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Size = new System.Drawing.Size(108, 31);
+            this.AnalyzeButton.TabIndex = 2;
+            this.AnalyzeButton.Text = "Analyze Folder";
+            this.AnalyzeButton.UseVisualStyleBackColor = true;
+            this.AnalyzeButton.Click += new System.EventHandler(this.AnalyzeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(729, 1035);
+            this.Controls.Add(this.AnalyzeButton);
+            this.Controls.Add(this.RichBox);
+            this.Controls.Add(this.PathInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox PathInput;
+        private System.Windows.Forms.RichTextBox RichBox;
+        private System.Windows.Forms.Button AnalyzeButton;
     }
 }
 
--- Folder-Analyzer/Folder-Analyzer/Form1.cs
+++ Folder-Analyzer/Folder-Analyzer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Folder_Analyzer
 {
@@ -16,5 +17,133 @@ namespace Folder_Analyzer
         {
             InitializeComponent();
         }
+
+        private void PathInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int count = 0;
+        private async void AnalyzeButton_Click(object sender, EventArgs e)
+        {
+            if (this.PathInput.Text == "")
+            {
+                this.RichBox.Clear();
+                this.RichBox.ForeColor = Color.Red;
+                this.RichBox.Text = "Path field is empty!";
+
+                await Task.Delay(2000);
+                this.RichBox.Clear();
+                this.RichBox.ForeColor = Color.White;
+            }
+            else
+            {
+                string[] Videos = { ".mp4", ".avi", ".mov", ".wmv", ".flv", ".mkv" };
+                string[] Docs = { ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".txt" };
+                string[] Pictures = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff" };
+                List<string> videoFiles = new List<string>();
+                List<string> docFiles = new List<string>();
+                List<string> pictureFiles = new List<string>();
+                string[] files = Directory.GetFiles(PathInput.Text);
+                int picturesTotal = 0;
+                int docsTotal = 0;
+                int videosTotal = 0;
+
+                for (int i = 0; i < Videos.Length; i++)
+                {
+                    string[] vids = Directory.GetFiles(this.PathInput.Text, $"*{Videos[i]}");
+                    videoFiles.AddRange(vids);
+                    videosTotal++;
+                }
+                string[] allVideos = videoFiles.ToArray();
+                int videosCount = videoFiles.Count;
+
+
+                for (int i = 0; i < Docs.Length; i++)
+                {
+                    string[] docs = Directory.GetFiles(this.PathInput.Text, $"*{Docs[i]}");
+                    docFiles.AddRange(docs);
+                    docsTotal++;
+                }
+                string[] allDocs = docFiles.ToArray();
+                int docsCount = docFiles.Count;
+
+
+                for (int i = 0; i < Pictures.Length; i++)
+                {
+                    string[] pics = Directory.GetFiles(this.PathInput.Text, $"*{Pictures[i]}");
+                    pictureFiles.AddRange(pics);
+                    picturesTotal++;
+                }
+                string[] allPictures = pictureFiles.ToArray();
+                int picturesCount = pictureFiles.Count;
+
+                this.RichBox.AppendText($"Total Files: {files.Length}" +
+                    $"\nDocs: {docsTotal}" +
+                    $"\nPictures: {picturesTotal}" +
+                    $"\nVideos: {videosTotal} \n");
+                this.RichBox.AppendText(new string('-', 50));
+
+                this.RichBox.AppendText("\n DOCS  | Copy Path: C:\\Users\\User\\Desktop\\Docs \n");
+                this.RichBox.AppendText(new string('-', 50));
+                for (int i = 0; i < allDocs.Length; i++)
+                {
+                    this.RichBox.AppendText("\n" + Path.GetFileName(allDocs[i]));
+                }
+
+                this.RichBox.AppendText("\n" + new string('-', 50));
+
+                this.RichBox.AppendText("\n PICTURES |Copy Path: C:\\Users\\User\\Desktop\\Pictures \n");
+                this.RichBox.AppendText(new string('-', 50));
+
+                for (int i = 0; i < allPictures.Length; i++)
+                {
+                    this.RichBox.AppendText("\n" + Path.GetFileName(allPictures[i]));
+                }
+
+                this.RichBox.AppendText("\n" + new string('-', 50));
+
+                this.RichBox.AppendText("\n VIDEOS |Copy Path: C:\\Users\\User\\Desktop\\Videos \n");
+
+                this.RichBox.AppendText(new string('-', 50));
+
+                for (int i = 0; i < allVideos.Length; i++)
+                {
+                    this.RichBox.AppendText($"\n {Path.GetFileName(allVideos[i])}");
+                }
+
+                string destDocs = "C:\\Users\\User\\Desktop\\Docs";
+                string destPics = "C:\\Users\\User\\Desktop\\Pictures";
+                string destVids = "C:\\Users\\User\\Desktop\\Videos";
+
+                string from = this.PathInput.Text;
+
+                for(int i = 0; i < allDocs.Length; i++)
+                {
+                    string DocsPath = allDocs[i];
+                    string DocsNames = Path.GetFileName(DocsPath);
+                    string dest = Path.Combine(destDocs, DocsNames);
+                    File.Copy(DocsPath, dest, true);
+                }
+                for (int i = 0; i < allPictures.Length; i++)
+                {
+                    string picsPath = allPictures[i];
+                    string picsNames = Path.GetFileName(picsPath);
+                    string dest = Path.Combine(destPics, picsNames);
+                    File.Copy(picsPath, dest, true);
+                }
+                for (int i = 0; i < allVideos.Length; i++)
+                {
+                    string vidPath = allVideos[i];
+                    string vidNames = Path.GetFileName(vidPath);
+                    string dest = Path.Combine(destVids, vidNames);
+                    File.Copy(vidPath, dest, true);
+                }
+            }          
+        }
+        }
     }
-}
